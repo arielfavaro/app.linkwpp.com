cdc6535c1668d8254aa2a85b13d84ade5ae204e2
Ariel Favaro <arieltainan@gmail.com>
2021-04-14 00:07:52 -0300

    version footer

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
             <div className="d-flex flex-column align-items-end justify-content-end">
                 <Link href="/politica-privacidade"><a>Política de Privacidade</a></Link>
                 {/* <span className="small d-block">Nenhum dado será armazenado</span> */}
-                <span className="small d-block">Ver. 0.1.7</span>
+                <span className="small d-block">Ver. 0.1.8</span>
             </div>
         </footer>
     )
