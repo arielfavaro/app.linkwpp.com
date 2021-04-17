cd7f8175ba28af6d2370090b116b411dcc8a7449
Ariel Favaro <arieltainan@gmail.com>
2021-04-17 10:08:03 -0300

    next config webpack 5, alias resolve

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
+const path = require('path')
 
 module.exports = withPWA({
+    future: { webpack5: true },
     pwa: {
         dest: 'public',
         runtimeCaching,
         sw: 'service-worker.js',
+    },
+    webpack: (config, options) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            '@': path.resolve(__dirname, './')
+        }
+        return config
     },
 })
