8a5530a18b0ac2aaadfa87bd9b40846680b41659
Ariel Favaro <arieltainan@gmail.com>
2021-04-17 17:00:20 -0300

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 function Footer() {
     return (
-        <footer className="container-fluid py-4">
+        <footer className="container-fluid py-5">
             <div className="d-flex flex-column align-items-end justify-content-end">
                 <Link href="/politica-privacidade"><a>Política de Privacidade</a></Link>
                 {/* <span className="small d-block">Nenhum dado será armazenado</span> */}
