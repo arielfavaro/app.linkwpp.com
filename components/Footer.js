bc32b1a7ce4d49fe12996b96d10b3b0d64ddf72f
Ariel Favaro <arieltainan@gmail.com>
2021-04-22 00:52:02 -0300

    footer updated

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,17 @@
 import Link from "next/link";
+// import { FaCoffee } from "react-icons/fa";
 
 function Footer() {
     return (
-        <footer className="container-fluid py-5">
-            <div className="d-flex flex-column align-items-end justify-content-end">
-                <Link href="/politica-privacidade"><a>Política de Privacidade</a></Link>
-                {/* <span className="small d-block">Nenhum dado será armazenado</span> */}
-                <span className="small d-block">Ver. 0.1.9</span>
+        <footer className="container-fluid mt-5 py-3 py-md-4 bg-secondary">
+            <div className="d-flex align-items-center justify-content-center pb-2">
+                <Link href="/politica-privacidade"><a className="mx-2">Política de Privacidade</a></Link>
+                <Link href="/termos"><a className="mx-2">Termos de Uso</a></Link>
+            </div>
+            <div className="d-flex flex-column align-items-center justify-content-center">
+                <span className="small d-block">Versão: 0.2.0</span>
+                <span className="small d-block">2021</span>
+                {/* <span className="small d-block mt-3">Made with <FaCoffee /> by <a href="https://arielfavaro.com/" target="_blank" className="text-white">Ariel Favaro</a></span> */}
             </div>
         </footer>
     )
